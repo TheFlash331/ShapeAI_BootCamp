550c739a38884ebd0e6d48c38abe9b2923c0236a
TheFlash331 <76915998+TheFlash331@users.noreply.github.com>
2021-06-28 22:21:59 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>JavaScript And React.js BootCamp</h1>
+      <p>
+        The Project demonstrating that i have learnt the basic JS syntax, React
+        Hello World Program and React Components.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
